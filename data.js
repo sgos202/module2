3dc55fa35768c9ddd1da954e583731044117e4f7
sgos202 <sumeetg2k11@hotmail.com>
2016-09-13 17:13:21 +1200

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 'use strict';
 
-$(function() {
+function slidecontrol() {
 
     //settings for slider
     var width = 720;
@@ -36,8 +36,29 @@ $(function() {
     startSlider();
 
 
-});
+};
 
-var x = setInterval(function(){
-	console.log(new Date());
-}, 1000);
+function api(){
+	var search_input = document.getElementById("search_bar").value;
+
+	var appKey = '75df820b4273e037da791510dc4d6114'
+	var appId = "628766d5"
+	var apiCall = "https://api.nutritionix.com/v1_1/search/" + search_input + "?results=0:20&fields=item_name,brand_name,item_id,nf_calories&appId=" + appId + "&appKey=" + appKey
+	
+	$(function(){
+		var $apiData = $('#apiData');
+
+		$.ajax({
+			type: 'GET',
+			url: 'https://api.nutritionix.com/v1_1/search/' + search_input + '?results=0:20&fields=*&appId=628766d5&appKey=75df820b4273e037da791510dc4d6114',
+			success: function(apiData) {
+				console.log("success", apiData);
+				$.each(apiData, function(i, item){
+					console.log('callx', i, item, apiData.hits);
+					$apiData.append('<li>Brand Name: '+ apiData.hits[1].fields.brand_name + ", Item Name: " + apiData.hits[1].fields.item_name + ", Carbohydrates: " + apiData.hits[0].fields.nf_total_carbohydrate + ", Calories: " + apiData.hits[1].fields.nf_calories + '</li>');
+				});
+
+		}
+	});
+	});
+};
